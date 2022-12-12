cf769676439f38c03fe3135913c982c0c81e94ad
Raghav G <83812669+PROG-007@users.noreply.github.com>
2022-12-12 18:21:58 +0530

    typo theek kara hain

--- that-wierd-list-sort.py
+++ that-wierd-list-sort.py
@@ -1,4 +1,4 @@
-#This code will output the list in ascending order of the digits the nesed tuples include......(1,2,3,123) = 6 digits
+#This code will output the list in ascending order of the digits the nested tuples include......(1,2,3,123) = 6 digits
 # Q16 of my monday test in class 11 second term 2022 xD
 
 a=[(1,2,3,765),(1,2),(234,345,456)]
